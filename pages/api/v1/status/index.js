a7a1b71777e37ffb750cd1d588a54ec69fc69d27
de-freitas <de-freitas@MacBookAir.bbrouter>
2025-01-26 13:51:46 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("select 1 + 1 as sum;");
+  console.log(result.rows[0].sum);
   response.status(200).json({ status: "everything working" });
 }
